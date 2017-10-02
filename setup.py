1e1ae78796c17604c21721deb6f46d3bfd8904b2
MarkusHaas <markushaas2003@gmail.com>
2017-10-02 14:49:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 import os
 os.system("chmod+x ARPSpoofer.py")
+ENTRY_POINTS = {"console_scripts": ["arpspoofer = arpspoofer:run"]}
 setup(
     name="ARPSpoofer",
     version="0.1",
     packages=find_packages(),
+    entry_points=ENTRY_POINTS,
 )
